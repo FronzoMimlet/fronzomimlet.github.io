3ba408990ecaa68db0fb81c47785e6f6fb04cc3e
Fraser M <frasermiller0@gmail.com>
2023-12-18 23:33:29 +0000

    Added a javascript functionality that loads the content only when all the images have loaded, change formatting of css to be more expc

---
+++ scrtips.js
@@ -0,0 +1,6 @@
+
+window.addEventListener("load", (event) => {
+  console.log("content loaded");
+  document.getElementById("loadingIndicator").style.display="none";
+  document.getElementById("contentToLoad").style.display="block";
+});
